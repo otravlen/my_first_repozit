349848e5718547e579049ca5af6bebee40b19294
Romashka <karmawayso@gmail.com>
2022-12-22 23:32:07 +0300

    ggg

--- maincun.py
+++ maincun.py
@@ -1,8 +1,18 @@
 import time
+import requests
+
 print('Hello, world')
 c = 0
-while c != 10:
+while c != 2:
     print('Privet mir')
-    time.sleep(2)
-    c += 1
+    c+= 1
     print('null')
+api_url = 'http://api.open-notify.org/iss-now.json'
+
+response = requests.get(api_url)   # Отправляем GET-запрос и сохраняем ответ в переменной response
+
+if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
+    print(response.text)
+    print(response.status_code)
+else:
+    print(response.status_code)    # При другом коде ответа выводим этот код
